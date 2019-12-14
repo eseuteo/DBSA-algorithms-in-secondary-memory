6dabbfe9b1f6f95a60fcf61758f59f68f5dbcf2d
Valdemar <valde1hs@hotmail.com>
2019-12-14 15:06:23 +0100

    Adding CombinedReadWrite function for read line and write mmap

--- CombinedReadWrite.py
+++ CombinedReadWrite.py
@@ -16,23 +16,30 @@ def rrmerge_Line_Line(file_list, outputFile):
                     file.isClosed = True
                 writeln_line(file_to_write, line)
 
-# Based on length_line
-def read_length_line_write_mmap(inputFile, outputFile, bufferSize, writePosition):
-    rFile = open(inputFile, 'r+b')
-    currentReadPosition = 0
+def rrmerge_line_mmap(inputFiles, outputFile, bufferSize, writePosition):
+    files_to_read = []
+    totalSize = 0
 
-    rFileSize = os.fstat(rFile.fileno()).st_size
+    for inputFile in inputFiles:
+        rFile = open(inputFile, 'r+b')
 
-    wFile = open(outputFile, 'r+b')
+        rFileSize = os.fstat(rFile.fileno()).st_size
+        totalSize += rFileSize
 
-    mapping, actualFilePosition, actualBufferSize = getNewMapRegion(writePosition, bufferSize, rFileSize, wFile, 1)
-
-    while True:
-        bline, currentReadPosition = readln_line(rFile, currentReadPosition)
-        if not bline:
-            break
-        mapping, writePosition, actualFilePosition = writeln_mmap(mapping, writePosition, actualFilePosition, actualBufferSize, rFileSize, wFile, bline)
+        files_to_read.append(FileObject(rFile, 0, False, None))
     
+    wFile = open(outputFile, 'w+b')
+
+    mapping, actualFilePosition, actualBufferSize = getNewMapRegion(writePosition, bufferSize, totalSize, wFile, 1)
+    
+    while not all([x.isClosed for x in files_to_read]):
+        for file in files_to_read:
+            if not file.isClosed:
+                bline, file.readPos = readln_line(file.fileObject, file.readPos)
+                if not bline:
+                    file.fileObject.close()
+                    file.isClosed = True
+                else:
+                    mapping, writePosition, actualFilePosition = writeln_mmap(mapping, writePosition, actualFilePosition, actualBufferSize, totalSize, wFile, bline)
     mapping.close
     wFile.close()
-    rFile.close()
