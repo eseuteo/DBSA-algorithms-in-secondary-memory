e4c967925531a7d30bcf0215a15ecd7fe2287112
eseuteo <ricardoholthausen@gmail.com>
2019-12-14 14:00:49 +0100

    rrmerge Line Line first version

--- CombinedReadWrite.py
+++ CombinedReadWrite.py
@@ -1,11 +1,20 @@
 from ReadWriteByMmap import getNewMapRegion, writeln_mmap
-from ReadWriteByLine import readln_line
+from ReadWriteByLine import readln_line, writeln_line
+from ClassFileObject import FileObject
 import os
 
 def rrmerge_Line_Line(file_list):
     files_to_read = []
+    file_to_write = open('output', 'r+b')
     for file in file_list:
-        files_to_read.append(jesus_objeto(file.open()))
+        files_to_read.append(FileObject(open(file, 'r+b'), 0, False))
+    while not all([x.isClosed for x in files_to_read]):
+        for file in files_to_read:
+            if not file.isClosed:
+                line, file.readPos = readln_line(file.fileObject, file.readPos)
+                if not line:
+                    file.isClosed = True
+                writeln_line(file_to_write, line)
 
 # Based on length_line
 def read_length_line_write_mmap(inputFile, outputFile, bufferSize, writePosition):
