1c9c8c0c03d5d7b0ecde2e2eb32c7524dba49638
jhuete <30612538+jhuete@users.noreply.github.com>
2019-12-14 15:25:38 +0100

    Added rrmerge_line_buffer

--- Experiment1_3.py
+++ Experiment1_3.py
@@ -3,7 +3,7 @@ import json
 import csv
 from pathlib import Path
 from time import time
-from CombinedReadWrite import rrmerge_Line_Line
+from CombinedReadWrite import rrmerge_Line_Line, read_length_line_write_mmap, rrmerge_line_buffer
 
 with open(str(Path.cwd())+'/Experiment1_3_parameters.json') as parametersFile:
     parameters = json.load(parametersFile)
@@ -14,4 +14,11 @@ filenames = []
 for filename in os.listdir(csv_folder):
     filenames.append(csv_folder + '/' + filename)
 
+# #--------------------------------------------------
+# # Merge Read_Line with Write_Line
 rrmerge_Line_Line(filenames, output_filename)
+
+# #--------------------------------------------------
+# # Merge Read_Line with Write_Buffer
+# bufferSize = 100
+# rrmerge_line_buffer(filenames, outputFilePath, bufferSize)
