29dc396989593e30cb781f572e6a4be32f094b56
eseuteo <ricardoholthausen@gmail.com>
2019-12-19 14:06:51 +0100

    Minor changes in Experiment 1.1

--- Experiment1_1.py
+++ Experiment1_1.py
@@ -15,19 +15,19 @@ field_names = ['implementation', 'file', 'length', 'running_time', 'buffer_size'
 writer = csv.DictWriter(output_file, fieldnames=field_names) 
 writer.writeheader()
 
-# for filename in os.listdir(csv_folder):
-#     print("Reading file " + filename + " with readln_char")
-#     start = time()
-#     result = length_char(csv_folder + '/' + filename)
-#     end = time()
-#     writer.writerow({'implementation':"Read by char", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size':'-'})
+for filename in os.listdir(csv_folder):
+    print("Reading file " + filename + " with readln_char")
+    start = time()
+    result = length_char(csv_folder + '/' + filename)
+    end = time()
+    writer.writerow({'implementation':"Read by char", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size':'-'})
 
-# for filename in os.listdir(csv_folder):
-#     print("Reading file " + filename + " with readln_line")
-#     start = time()
-#     result = length_line(csv_folder + '/' + filename)
-#     end = time()
-#     writer.writerow({'implementation':"Read by line", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size':'-'})
+for filename in os.listdir(csv_folder):
+    print("Reading file " + filename + " with readln_line")
+    start = time()
+    result = length_line(csv_folder + '/' + filename)
+    end = time()
+    writer.writerow({'implementation':"Read by line", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size':'-'})
 
 for filename in os.listdir(csv_folder):
     print("Reading file " + filename + " with readln_buffer")
@@ -38,11 +38,11 @@ for filename in os.listdir(csv_folder):
         end = time()
         writer.writerow({'implementation':"Read by buffer", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size': str(buffer_size)})
 
-# for filename in os.listdir(csv_folder):
-#     print("Reading file " + filename + " with readln_mmap")
-#     for buffer_size in [2 ** x for x in range(0, 15)]:
-#         print("\tbuffer size " + str(buffer_size))
-#         start = time()
-#         result = length_mmap(csv_folder + '/' + filename, buffer_size)
-#         end = time()
-#         writer.writerow({'implementation':"Read by mmap", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size': str(buffer_size)})
+for filename in os.listdir(csv_folder):
+    print("Reading file " + filename + " with readln_mmap")
+    for buffer_size in [2 ** x for x in range(0, 15)]:
+        print("\tbuffer size " + str(buffer_size))
+        start = time()
+        result = length_mmap(csv_folder + '/' + filename, buffer_size)
+        end = time()
+        writer.writerow({'implementation':"Read by mmap", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size': str(buffer_size)})
