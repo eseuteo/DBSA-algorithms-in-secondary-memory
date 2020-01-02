9d12e5147c39eaa53ba37ccc1395974302d5658c
jhuete <30612538+jhuete@users.noreply.github.com>
2020-01-02 19:48:16 +0100

    Final fix push

--- Experiment1_4.py
+++ Experiment1_4.py
@@ -5,12 +5,14 @@ from pathlib import Path
 from time import time
 from MultiWayMerge import extsort_Line_Line
 
-with open(str(Path.cwd())+'/input/Experiment1_4_parameters.json') as parametersFile:
+with open(str(Path.cwd()) + '/input/Experiment1_4_parameters.json') as parametersFile:
+# with open('C:\\Users\\a0081\\Documents\\BDMA\\ULB\\DB Systems Archutecture INFOH417\\DBSA_all_files\\input\\Experiment1_4_parameters.json') as parametersFile:
     parameters = json.load(parametersFile)
     csv_folder = parameters['csv_folder']
-    output_filename = 'output/' + parameters['output_filename']
+    output_filename = '/output/' + parameters['output_filename']
 
-output_file = open(str(output_filename) + '.csv', 'w', newline='')
+output_file = open(str(Path.cwd()) + str(output_filename), 'w', newline='')
+# output_file = open(str(output_filename) + '.csv', 'w', newline='')
 field_names = ['implementation', 'file', 'length', 'running_time', 'buffer_size']
 writer = csv.DictWriter(output_file, fieldnames=field_names) 
 writer.writeheader()
@@ -20,14 +22,11 @@ writer.writeheader()
 for filename in os.listdir(csv_folder):
     print("Merge-sort file " + filename + " with readln_line and writeln_line")
     f = csv_folder + '/' + filename # File to sort
-    k = 4 # Sort on kth column
+    k = 2 # Sort on kth column
     M = 512000 # Number of bytes of available memory
-    d = 3 # Number of streams to merge
+    d = 5 # Number of streams to merge
     start = time()
     result = extsort_Line_Line(f, k, M, d)
     end = time()
     writer.writerow({'implementation':"Read_Line_Write_Line", 'file':filename, 'length': str(result), 'running_time': str(end - start), 'buffer_size':'-'})
-
-
-
 
